a2402a8bfee6ed8168769de0511d323c30253a70
Felipe Signorini <felipeklerk@yahoo.com.br>
2019-05-11 12:52:32 +0100

    enable tls

--- run_start_container.sh
+++ run_start_container.sh
@@ -8,17 +8,29 @@ VADSECRET=$CENTRIFUGO_ADMIN_SECRET
 VADMIN=${VADMIN:='-'}
 VADSECRET=${VADSECRET:='-'}
 
+CWPATH=".config.json"
+
 jq -n\
  --arg secret $VSECRET\
  --arg api_key $VAPIPKEY\
  --arg admin_password $VADMIN\
  --arg admin_secret $VADSECRET\
- '{"secret":$secret, "api_key":$api_key, "admin_password":$admin_password, "admin_secret":$admin_secret}' > /config.json
+ '{"secret":$secret, "api_key":$api_key, "admin_password":$admin_password, "admin_secret":$admin_secret}' > $CWPATH
+
+
+if [ -z ${CENTRIFUGO_TLS} ];
+    then
+        echo "-> Tls disabled"
+    else
+        echo "-> Tls enabled"
+        TMPJ=$(jq '.tls_autocert=true | .tls_autocert_cache_dir = "\/tmp\/certs"' $CWPATH)
+        echo $TMPJ > $CWPATH
+fi
 
 if [ -z ${CENTRIFUGO_ADMIN} ] && [ -z ${CENTRIFUGO_ADMIN_SECRET} ]; 
     then 
         centrifugo -c /config.json
     else
-        echo "Centrifugo Admin UP";
+        echo "-> Centrifugo Admin UP";
         centrifugo -c /config.json --admin
 fi
